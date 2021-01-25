d72c785e6f3bdce6e9374bf3fbd9afcc8afb486c
szachMati <donek8@gmail.com>
2021-01-25 22:37:39 +0100

    dumps lepszy od jsonify xD

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import datetime
-from flask import Flask, Response, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, Response, request
+from flask_cors import CORS
 from pymongo import MongoClient
 from .utils import deserialize_json
 from .model import User
 from .utils.settings import MONGO_URL, JWT_SECRET_KEY
+from json import dumps
 
 app = Flask(__name__)
 CORS(app)
@@ -15,20 +16,19 @@ db = client.weather_db
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
+
 @app.route('/api/signup', methods=['POST'])
-@cross_origin('*')
 def signup():
     user: User = deserialize_json(request.data)
-    print(user)
-    # if db.users.find_one({"email": user.name}) is not None:
-    #     return Response(response=jsonify({"error": "User already exists"}), status=409)
+    if db.users.find_one({"email": user.name}) is not None:
+        return Response(response=dumps({"error": "User with given email already exists"}), status=409)
     db.users.insert_one({
         "name": user.name,
         "surname": user.surname,
         "email": user.email,
         "password": user.password  # tutaj jakiś bcrypt by się przydał :p
     })
-    return Response(mimetype='application/json', status=200, response=jsonify())
+    return Response(status=200, response=dumps({"message": "Registration completed successfully"}))
 
 
 @app.route('/api/hello-world')
