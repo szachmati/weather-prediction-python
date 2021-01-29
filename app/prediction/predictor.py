a7377a12606b44a0c8f17c54dbd291f8766dcba1
duudey <jduda1997@gmail.com>
2021-01-29 19:47:26 +0100

    new version of predictor v2

--- app/prediction/predictor.py
+++ app/prediction/predictor.py
@@ -1,159 +1,115 @@
-import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score
-import joblib
-from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import StandardScaler
-import csv
-import datetime
-from math import sqrt
-from sklearn.svm import SVR
-import sklearn.svm as svm
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from datetime import timedelta
+import numpy as np
+from keras.models import Sequential
+from keras.layers import *
+from sklearn.preprocessing import MinMaxScaler
+import time
+import dataCreator
+from datetime import date, timedelta
 
-column_names = [
-    'Fran Datum Tid (UTC)', 'till', 'day', 'temperature', 'Kvalitet', 'Tidsutsnitt:', 'Unnamed: 5'
-]
-column_names_used = [
-    'Fran Datum Tid (UTC)', 'till', 'day'
-]
+def predict(miasto, parameter) :
+    #tworzy się objekt Dataframe z zawartością danych historycznych ze wskazanego miasta
+    df = pd.read_csv(miasto + '.csv', sep=',')
+    kol_od = 19
+    kol_do = 20
 
+    df.head(5)
+    #pobieranych jest index, który będzie przedstawiał
+    split_idx = int(len(df) * 0.9)
+    training_set = df.iloc[:split_idx, kol_od:kol_do].values
+    test_set = df.iloc[split_idx:, kol_od:kol_do].values
 
-def make_numeric_values(arr, title):
-    new_arr = []
-    for date in arr[title]:
-        new_date = make_date(date)
-        new_arr.append(new_date)
-    arr[title] = new_arr
+    # Feature Scaling
+    sc = MinMaxScaler(feature_range=(0, 1))
+    training_set_scaled = sc.fit_transform(training_set)
 
+    X_train, X_train2, X_train3 = [], [], []
+    y_train, y_train2, y_train3 = [], [], []
 
-def fix_array(arr):
-    for name in column_names_used:
-        make_numeric_values(arr, name)
+    for i in range(60, split_idx):
+        X_train.append(training_set_scaled[i - 50:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+    # =========================================================
+    X_train, y_train = np.array(X_train), np.array(y_train)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
+    EPOCHS = 100
+    BATCH_SIZE = 5
+    model = Sequential()
+    # Dodajemy 1 warstwę
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    # Dodajemy 2 warstwę
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    # Dodajemy 3 warstwę
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    # Dodajemy 4 warstwę
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    # Adding the output layer
+    model.add(Dense(units=1))
+    # Compiling the RNN
+    start = time.time()
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
-def make_date(date):
-    new_date = date.split(' ')
-    new_date = new_date[0]
-    new_date = new_date.split('-')
-    new_number = ''
-    first = True
-    for number in new_date:
-        if first:
-            first = False
-        else:
-            new_number = new_number + number
-    return new_number
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE)
 
+    dataset_train = df.iloc[:split_idx, kol_od:kol_do]
+    dataset_test = df.iloc[split_idx:, kol_od:kol_do]
 
-def convert_date_to_string(plus_days):
-    date = datetime.datetime.today() + timedelta(days=plus_days)
-    date = date.strftime("%Y-%m-%d %H:%M:%S")
-    date = date.split(' ')
-    date = date[0]
-    date = date.split('-')
-    date = date[1] + date[2]
-    return date
+    dataset_total = pd.concat((dataset_train, dataset_test), axis=0)
 
+    inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+    inputs = inputs.reshape(-1, 1)
+    inputs = sc.transform(inputs)
 
-# THIS IS WHERE THE MODEL GETS TRAINED
-# if you want to use this in your own project this is the method you want to study
+    X_test, X_test2, X_test3 = [], [], []
 
-def train():
-    dataset_url1 = 'https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/2/station/71420/period/corrected-archive/data.csv'
-    dataset_url2 = 'https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/2/station/71420/period/latest-months/data.csv'
-    dataset_warsaw = '.'
+    for i in range(60, len(inputs)):
+        X_test.append(inputs[i - 50:i, 0])
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-    data1 = pd.read_csv(dataset_url1, sep=';', skiprows=3607, names=column_names)
-    data2 = pd.read_csv(dataset_url2, sep=';', skiprows=15, names=column_names)
+    print(X_test.shape)
 
-    data1 = data2.append(data1)
-    data1 = data1.drop('Tidsutsnitt:', axis=1)
-    X = data1.drop(["temperature"], axis=1)
-    X = X.drop(['Kvalitet'], axis=1)
-    X = X.drop(['Unnamed: 5'], axis=1)
-    fix_array(X)
+    predicted_param = model.predict(X_test)
+    predicted_param = sc.inverse_transform(predicted_param)
+    end = time.time()
+    elapsed = end - start
 
-    y = data1['temperature']
+    print(df.loc[split_idx:, 'date_time'])
+    plt.plot(df.loc[split_idx:, 'date_time'], dataset_test.values, color='black', label='Real TempC')
+    plt.plot(df.loc[split_idx:, 'date_time'], predicted_param, color='orange', label='Predicted TempC')
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+    plt.xticks(np.arange(0, len(dataset_test.values), 5), rotation='vertical')
+    plt.title(' Prediction EPOCHS = ' + str(EPOCHS) + ' BATCH_SIZE = ' + str(BATCH_SIZE) + ' INPUT_DATA = ' + str(
+        len(df)) + ' CALCULATION TIME: ' + str(round(elapsed, 4)) + 's')
+    plt.xlabel('Time')
+    plt.ylabel('parameters')
+    plt.legend()
+    plt.show()
 
-    tree_model = DecisionTreeRegressor()
+def getEndDate():
+    return date.today().strftime("%d-%b-%Y")
 
-    tree_model.fit(X_train, y_train)
-    joblib.dump(tree_model, 'weather_predictor.pkl')
-    print("-" * 48)
-    print("\nDone training\n")
-    print("-" * 48)
-
-
-def predict_weather():
-    tree_model = joblib.load('weather_predictor.pkl')
-
-    print("-" * 48)
-    print("Enter the details of the date you would like to predict")
-    print("\n")
-    option = input("Year: ")
-    year = option
-    option = input("Month number (00): ")
-    month = option
-    option = input("Day number (00): ")
-    theday = option
-
-    day = str(month) + str(theday)
-
-    date = [
-        [day,
-         (str(int(day) + 1)),
-         (day)]
-    ]
-    temp = tree_model.predict(date)[0]
-    print("-" * 48)
-    print("\nThe temperature is estimated to be: " + str(temp) + "\n")
-    print("-" * 48)
-
-
-def run_program(option):
-    if option == '1':
-        print("1")
-    elif option == '2':
-        predict_weather()
-
+def getStartDate():
+    return (date.today() - timedelta(days= 729)).strftime("%d-%b-%Y")
 
 def run_menu():
-    print("*" * 48)
-    print("-" * 10 + " What would you like to do? " + "-" * 10)
-    print("\n")
-    print("1. Look up the weather on a specific day")
-    print("2. Predict the weather on a specific day")
-    print("\n")
-
-    option = input("Enter option: ")
-
-    while True:
-        if option == '2' or option == '1' or option == '9' or option == '3':
-            break
-        option = input("Enter option: ")
-    return option
-
-
-if __name__ == "__main__":
-    # X is our input variables that will be used to predict y which is our output so temperature
-    # the data in X needs to be converted to numeric values to simplify our process
-    while True:
-        option = run_menu()
-        if option == '9':
-            break
-        if option == '3':
-            train()
-        else:
-            run_program(option)
+    print("*" * 67)
+    print("-" * 22 + "5 DAYS WEATHER FORECAST" + "-" * 22)
+    print(" " * 5 + " Write name of the city (e.g. Warsaw, Gdynia, London)" + " " * 5)
+    cityName = input("Enter city name: ")
+    dataCreator.retrieve_hist_data([cityName], getStartDate(), getEndDate(), 24, location_label=False, export_csv=True, store_df=True)
+    print("-" * 67)
+    print(" " * 5 + "Program can predict the following parameters for " + cityName + " " * 5)
+    print(" " * 3 + "|tempC     |maxtempC  |mintempC|totalSnow_cm|FeelsLikeC   |")
+    print(" " * 3 + "|HeatIndexC|WindChillC|humidity|pressure    |windspeedKmph|")
+    parameter = input("Enter parameter name: ")
+    print("-" * 67)
+    print(" " * 5 + "Program now is now making forecast of " + parameter + " for next 5 days in " + cityName + " " * 5)
+    predict(cityName,parameter)
+    print("*" * 67)
